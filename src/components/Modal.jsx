33c530786cf84fbba084252ffc25d0b70c1b336b
mrouw <bchva@yandex.ru>
2024-07-27 19:27:02 +0700

    drop component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -14,14 +14,10 @@ function NewModal() {
 
     const modalContent = (
         <div>
-        <h2>Заголовок модального окна</h2>
-        <p>Текст модального окна</p>
-        <button onClick={closeModal}>Закрыть</button>
         </div>
     );
     return (
     <div>
-        <button onClick={openModal}>Открыть модальное окно</button>
         <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
         {modalContent}
         </Modal>
